1d29d9e6ac19682264fd8e82124ca182e6b0b9d3
erickrods <98923361+erickrods@users.noreply.github.com>
2022-04-20 00:33:43 -0300

    Update interfaces.java

--- Interfaces/interfaces.java
+++ Interfaces/interfaces.java
@@ -38,14 +38,14 @@ public class Bala implements veloz {
 	}
 }
 
-2 - espaço(Apartamento, Armario, Geladeira)
+2 - Dimensional(Apartamento, Armario, Geladeira)
 		areaInterna(Calcula e retorna area interna do objeto.)
 		compartimentos(Coletar nome dos diferentes ambientes do objeto.)
-public interface espaco{
+public interface Dimensional{
 	public int areaInterna();
 	public String compartimentos(); 		
 }
-public class Apartamento implements espaco{
+public class Apartamento implements Dimensional{
 	public int areaInterna(){
 		//Calculos para definir a area interna do apartamento e retornar valor encontrado. 
 		return area;
@@ -56,7 +56,7 @@ public class Apartamento implements espaco{
 		return ambientes;
 	}
 }
-public class Armario implements espaco{
+public class Armario implements Dimensional{
 	public int areaInterna(){
 		//Calculos para definir a area interna do armario e retornar valor encontrado. 
 		return area;
@@ -67,7 +67,7 @@ public class Armario implements espaco{
 		return ambientes;
 	}
 }
-public class Geladeira implements espaco{
+public class Geladeira implements Dimensional{
 	public int areaInterna(){
 		//Calculos para definir a area interna da geladeira e retornar valor encontrado. 
 		return area;
