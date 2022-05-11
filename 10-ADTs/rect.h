0bcb62e1cba023b0a3fd7f6269b6e720841ad15c
erickrods <98923361+erickrods@users.noreply.github.com>
2022-05-11 00:33:40 -0300

    Revert "adt"
    This reverts commit c86015c8ee729c5d0e9f0d5b9dfb6ebcbe0b26a9.

--- 10-ADTs/rect.h
+++
@@ -1,6 +0,0 @@
-typedef struct Rect Rect;
-Rect* Rect_new(int, int, int, int);
-void Rect_print(Rect*);
-void Rect_drag(Rect*, int dx, int dy);
-typedef struct Figure Figure;
-typedef void (*Figure_print) (struct Figure*);
